454056b82ded6057fc3657c2acb563ca6fc7dcec
tldms <tldms201@naver.com>
2020-11-20 19:37:41 +0900

    TTS - input Language : Korean

--- app/src/main/java/com/example/smartmirror/FreeTalking.java
+++ app/src/main/java/com/example/smartmirror/FreeTalking.java
@@ -49,7 +49,7 @@ public class FreeTalking extends AppCompatActivity {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko_KR");
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi speak something");
 
         try{
