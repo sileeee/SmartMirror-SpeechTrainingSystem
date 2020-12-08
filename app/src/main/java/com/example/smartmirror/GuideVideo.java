e5d6335619936bb27a9f4721d58c12797747f783
Hoooon22 <momo990305@gmail.com>
2020-12-09 01:15:59 +0900

    video no?

--- app/src/main/java/com/example/smartmirror/GuideVideo.java
+++ app/src/main/java/com/example/smartmirror/GuideVideo.java
@@ -2,47 +2,59 @@ package com.example.smartmirror;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.res.Resources;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class GuideVideo extends AppCompatActivity {
-
+    VideoView vv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide_video);
 
-        final VideoView testVideo = (VideoView)findViewById(R.id.testVideo);
-        Resources res = getResources();
-        int id_video = res.getIdentifier("test","raw", getPackageName());
+        vv= findViewById(R.id.vv);
+        //Video Uri
+        Uri videoUri= Uri.parse("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4");
+        // 자체 경로
+        Uri introURI;
+        introURI = Uri.parse("android.resource://your.app.package/" + R.raw.test);
 
-        Uri uri = Uri.parse("android.resource://com.example.test/" + id_video);
 
-        // 해당하는 비디오 uri 장착
-        testVideo.setVideoURI(uri);
-        // 비디오 시작
-        testVideo.start();
-        // 화면 아래에 비디오 컨트롤러 만들기
-        MediaController mcontroller = new MediaController(this);
-        testVideo.setMediaController(mcontroller);
-        testVideo.setOnTouchListener(new View.OnTouchListener() {
-            // 터치 시 비디오 정지, 시작 처리
+        //비디오뷰의 재생, 일시정지 등을 할 수 있는 '컨트롤바'를 붙여주는 작업
+        vv.setMediaController(new MediaController(this));
+
+        //VideoView가 보여줄 동영상의 경로 주소(Uri) 설정하기
+        vv.setVideoURI(videoUri);
+
+        //동영상을 읽어오는데 시간이 걸리므로..
+        //비디오 로딩 준비가 끝났을 때 실행하도록..
+        //리스너 설정
+        vv.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (testVideo.isPlaying()) { // 비디오의 재생여부를 boolean 값으로 반환.
-                    testVideo.pause();
-                    return false;
-                } else {
-                    testVideo.start();
-                    return false;
-                }
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                //비디오 시작
+                vv.start();
             }
         });
 
+    }//onCreate ..
+
+    //화면에 안보일때...
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //비디오 일시 정지
+        if(vv!=null && vv.isPlaying()) vv.pause();
+    }
+    //액티비티가 메모리에서 사라질때..
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //
+        if(vv!=null) vv.stopPlayback();
     }
 }
