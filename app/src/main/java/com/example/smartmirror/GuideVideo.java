d762c50b7aadd6f44fe58d4306ee61faef911d19
tldms <tldms201@naver.com>
2021-01-28 16:32:39 +0900

    Add Spinner in GuideVideo Page

--- app/src/main/java/com/example/smartmirror/GuideVideo.java
+++ app/src/main/java/com/example/smartmirror/GuideVideo.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.MediaController;
+import android.widget.Spinner;
 import android.widget.VideoView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -51,6 +52,17 @@ public class GuideVideo extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guide_video);
 
+        final TextView textView = (TextView)findViewById(R.id.textView);
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
+                textView.setText("현재 예제 비디오 : " + parent.getItemAtPosition(position));
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent){}
+        });
         Button_event = (Button)findViewById(R.id.list);
 
         vv = findViewById(R.id.vv);
