edca6ddc150b3449eff18888e032219ece550ba8
tldms <tldms201@naver.com>
2021-01-29 23:53:54 +0900

    create back button

--- app/src/main/java/com/example/smartmirror/Analysis.java
+++ app/src/main/java/com/example/smartmirror/Analysis.java
@@ -2,10 +2,16 @@ package com.example.smartmirror;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Analysis extends AppCompatActivity {
 
+    public void ClickBtnBack(View view) {
+        Intent intent = new Intent( this, GuideVideo.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
